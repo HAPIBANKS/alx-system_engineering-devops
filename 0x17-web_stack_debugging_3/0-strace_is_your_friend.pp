882ba31ea66675cecffd17c922204341f37954b5
HAPIBANKS <posiayo1@gmail.com>
2024-06-04 20:06:26 +0100

    strace

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Using strace, find out why Apache is returning a 500 error
+# fix it and then automate it using Puppet
+exec {'sed phpp':
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => 'test -f /var/www/html/wp-settings.php',
+}
