d37fef7f8121d25f383cf2a49938c9927de80334
HAPIBANKS <posiayo1@gmail.com>
2024-06-07 13:15:29 +0100

    how many sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,18 +4,23 @@
 a function that queries the Reddit API and returns the number of subscribers.
 """
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
     """Return the number of subscribers for a given subreddit."""
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    url = get("https://www.reddit.com/r/{}/about.json".format(subreddit)
+              params={"raw_json": 1},
+              headers={"user-Agent": "HAPI from ALX"},
+              allow_redirects=False)
+
+    try:
+        url.raise_for_status()
+    except:
         return 0
+    else:
+        numb_of_sub = url.json().get('data').get('subscribers')
+        if numb_of_sub is None:
+            return 0
+        return numb_of_sub
