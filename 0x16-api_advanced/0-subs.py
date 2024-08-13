b7b2cb79d2d64895ad86bc0ab7151a48507a066b
HAPIBANKS <posiayo1@gmail.com>
2024-08-13 16:00:44 +0100

    sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,17 +10,16 @@ import requests
 
 def number_of_subscribers(subreddit):
     """base url"""
-    resp = get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
-               params={"raw_json": 1},
-               headers={"User-Agent": "Andrew from Alx"},
-               allow_redirects=False)
-
     try:
+        response = requests.get(
+            'https://www.reddit.com/r/{}/about.json'.format(subreddit),
+            params={"raw_json": 1},
+            headers={"User-Agent": "Posiayo from Alx"},
+            allow_redirects=False)
         response.raise_for_status()
-    except:
-        return 0
-    else:
-        number_of_sub = response.json().get('data').get('subscribers')
+        number_of_sub = response.json().get('data', {}).get('subscribers')
         if number_of_sub is None:
             return 0
         return nuber_of_sub
+    except requests.exceptions.RequestException:
+        return 0
